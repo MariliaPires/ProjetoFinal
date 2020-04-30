87546a500491a19a2f10e4976c1e7853734b66e5
GustavoDZ <gdiniz@outlook.com>
2020-04-30 11:37:59 -0300

    Front atualizado
    Front atualizado

--- Front/novopedido.js
+++ Front/novopedido.js
@@ -1,3 +1,9 @@
+
+function LoadPage(){
+    carregaMaquinas();
+    carregaSoftwares();
+}
+
 function carregaSoftwares(){
     // preciso ver se o usuario ta logado
     var user = localStorage.getItem("user");
@@ -8,9 +14,23 @@ function carregaSoftwares(){
         fetch('http://localhost:8080/softwares/disponiveis')
            .then(res=>res.json())
            .then(res=>popula(res));
-    }
-    // 
+    } 
 }
+
+
+function carregaMaquinas(){
+    // preciso ver se o usuario ta logado
+    var user = localStorage.getItem("user");
+    if (!user){
+        window.location="index.html";
+    }
+    else{
+        fetch('http://localhost:8080/maquinas')
+           .then(res=>res.json())
+           .then(res=>populaM(res));
+    } 
+}
+
 
 function popula(lista){
     var strSoftware = "";
@@ -20,12 +40,44 @@ function popula(lista){
     document.getElementById("listasw").innerHTML = strSoftware;
 }
 
+function populaM(lista){
+    var strMaquina = "<select>";
+    for (i=0; i<lista.length; i++){
+        strMaquina += "<option value='"+lista[i].id+"'>"+lista[i].descricao +"</option>";
+    }
+    strMaquina += "</select>"
+    document.getElementById("listasM").innerHTML = strMaquina;
+}
+
 function confirmar(){
     var listaItens = document.getElementsByTagName("input");
+    var strBody = {  "status":"N",
+    "observacoes":"Primeiro pedido efetuado colab Jose",} 
+    var d = new Date();
+    console.log(d.getDate());
+    console.log(parseInt(d.getMonth())+1);
+    console.log(d.getFullYear());
+    var strdate = d.getDate()+"/"+(parseInt(d.getMonth())+1)+"/"+d.getFullYear();
+
+    console.log(strdate);
     for (i=0; i<listaItens.length;i++){
+ 
         var x = document.getElementById(i).checked;
        if(x){
-        console.log(listaItens[i].value);
+        console.log(parseInt(listaItens[i].id)+1);
        }
     }
+    /*{
+        "status":"N",
+        "observacoes":"Primeiro pedido efetuado colab Jose",
+        "dataPedido":"27/04/2020",
+        "solicitante":{
+            "id":2
+        },
+        "itens":[
+            { "itemSoftware": {"id": 1 } },
+            { "itemSoftware": {"id": 2 } },
+            { "itemSoftware": {"id": 5 } }
+        ]
+    }*/
 }
